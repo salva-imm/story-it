ac96becfcb9497b306f64ebdc81a702caa222639
salvador <mehdi73ee@gmail.com>
2021-02-18 04:33:45 +0330

    a complete endpoint view with validation and returning response

--- backend/user/api/views.py
+++ backend/user/api/views.py
@@ -1,27 +1,38 @@
-from pydantic import BaseModel, constr
-from starlette.responses import JSONResponse
-
 from user.models import User
+from pydantic import BaseModel, constr
+from tortoise.exceptions import DoesNotExist
 
 from utils.base import BaseEndpoint
+from starlette.responses import JSONResponse
+from starlette.endpoints import HTTPException
+
+from .serializers import UsersSerializer
 
 
 class UserRegister(BaseEndpoint):
     class Arguments(BaseModel):
-        username: constr(max_length=2)
+        username: constr(max_length=128)
         password: constr(max_length=128)
         email: constr(max_length=128)
 
     class Validator:
-        def validate_username(self):
-            # TODO: check username uniqueness
-            pass
+        async def validate_username(self, username):
+            try:
+                await User.get(username=username.lower())
+                raise HTTPException(detail="Username already exist!", status_code=400)
+            except DoesNotExist:
+                pass
 
-        def validate_email(self):
-            # TODO: check email uniqueness
-            pass
+        async def validate_email(self, email):
+            try:
+                await User.get(email=email.lower())
+                raise HTTPException(detail="Email already exist!", status_code=400)
+            except DoesNotExist:
+                pass
 
     async def post(self, request):
         data = await request.json()
-        self.is_valid(**data)
-        return JSONResponse({"data": f"Hello, world!"})
+        await self.is_valid(**data)
+        user = await User.create(**data)
+        res = UsersSerializer.get_response(user)
+        return JSONResponse({"data": res})
