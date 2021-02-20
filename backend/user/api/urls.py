e21a743cf6287da9ac6e2ffc41713dbd906d0032
salvador <mehdi73ee@gmail.com>
2021-02-21 02:50:03 +0330

    jwt auth endpoint with url

--- backend/user/api/urls.py
+++ backend/user/api/urls.py
@@ -1,7 +1,10 @@
-from .views import UserRegister
+from .views import (
+    UserRegister, Auth
+)
 from starlette.routing import Route
 
 
 routes = [
     Route('/user/register', UserRegister),
+    Route('/user/auth', Auth),
 ]
